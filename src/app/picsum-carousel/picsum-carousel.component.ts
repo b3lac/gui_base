4e57fb39ac9a0519ae2b9413b4b7dd0c9ea554dd
glasgow <glasgowindustries@gmail.com>
2018-04-26 00:11:41 -0500

    Fixed carousel by using local images. added 3 images to assets

--- src/app/picsum-carousel/picsum-carousel.component.ts
+++ src/app/picsum-carousel/picsum-carousel.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {map} from 'rxjs/operators';
 import {NgbCarousel} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -9,19 +7,10 @@ import {NgbCarousel} from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./picsum-carousel.component.scss']
 })
 export class PicsumCarouselComponent implements OnInit {
-  images: Array<string>;
-  constructor(private _http: HttpClient) {}
+
+  constructor() {}
 
   ngOnInit() {
-    this._http.get('https://picsum.photos/list')
-    .pipe(map((images: Array<{id: number}>) => this._randomImageUrls(images)))
-    .subscribe(images => this.images = images);
-  }
-  private _randomImageUrls(images: Array<{id: number}>): Array<string> {
-    return [1, 2, 3].map(() => {
-      const randomId = images[Math.floor(Math.random() * images.length)].id;
-      return `https://picsum.photos/900/500?image=${randomId}`;
-    });
   }
 
 }
