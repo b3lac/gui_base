ef381758dd3a5abf8644181c15d4e2b2b96a24a6
b3lac <b3lac.g@gmail.com>
2018-05-05 03:08:29 -0500

    Clean slate on components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { OneColComponent } from './layouts/one-col/one-col.component';
-import { TwoColComponent } from './Layouts/two-col/two-col.component';
-import { ThreeColComponent } from './Layouts/three-col/three-col.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    OneColComponent,
-    TwoColComponent,
-    ThreeColComponent
   ],
   imports: [
     BrowserModule
