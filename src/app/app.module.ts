cd7830e7cdbc66fed46d42a958a1b4bf32d650a9
b3lac <b3lac.g@gmail.com>
2018-05-05 00:27:40 -0500

    Generated footer, header, and middle sections to be used by Layouts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { OneColComponent } from './layouts/one-col/one-col.component';
+import { TwoColComponent } from './Layouts/two-col/two-col.component';
+import { ThreeColComponent } from './Layouts/three-col/three-col.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OneColComponent,
+    TwoColComponent,
+    ThreeColComponent
   ],
   imports: [
     BrowserModule
